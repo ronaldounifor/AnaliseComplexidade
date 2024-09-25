d70698d363d016b470c5d0d33f4554d45684056f
professord14 <ronaldo@unifor.br>
2024-09-24 21:48:38 -0300

    Exercicio 2 feito e inicio do 3

--- src/App.java
+++ src/App.java
@@ -2,10 +2,10 @@ public class App {
     public static void main(String[] args) {
         new GerenteMatriz(
             new int[][]{
-                {2, 9, 5},
                 {3, 2, 1},
+                {2, 9, 5},
                 {2, 7, 8}
             }
-        ).procurar(2);
+        ).procurarUm(3);
     }
 }
--- src/GerenteMatriz.java
+++ src/GerenteMatriz.java
@@ -23,4 +23,25 @@ public class GerenteMatriz {
                 if(elemento == matriz[linha][coluna]) //                                            1       n*n = n²
                     System.out.println("Achei na linha " + (linha+1) + " e coluna "+(coluna+1));
     }
+
+    /**
+     * Análise de complexidade de tempo do melhor caso para o método procurarUm
+     * 
+     * T(n) = 1 + 1 + 1 = 3 = Ω(1)
+     * 
+     * 3 = Ω(1)
+     * 
+     * 3 ≥ c*1
+     * 3 ≥ 3*1
+     * 
+     * c = 3, k = 1
+     */
+    public void procurarUm(int elemento) { //                                                     #comps  #vezes
+        for (int linha = 0; linha < matriz.length; linha++) //                                    1       1
+            for (int coluna = 0; coluna < matriz.length; coluna++) //                             1       1
+                if(elemento == matriz[linha][coluna]) { //                                        1       1
+                    System.out.println("Achei na linha " + (linha+1) + " e coluna "+(coluna+1));
+                    return;
+                }
+    }
 }
